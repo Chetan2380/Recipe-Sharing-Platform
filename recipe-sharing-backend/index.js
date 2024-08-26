bd07a8e04f176b02c19ba2a28284059cbd312a32
Chetan2380 <chetaningale2398@gmail.com>
2024-08-26 19:40:00 +0530

    Update

--- recipe-sharing-backend/index.js
+++ recipe-sharing-backend/index.js
@@ -12,7 +12,7 @@ app.use(morgan("combined"));
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000", "https://recipe-sharing-platform-sand.vercel.app"],
+    origin: ["https://recipe-sharing-platform-sand.vercel.app"],
   })
 );
 dotenv.config();
